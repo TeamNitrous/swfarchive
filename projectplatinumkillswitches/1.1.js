c0fccf06fbf4e98362723697110b9fcce80de761
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-04-16 14:06:28 -0700

    Update and rename 1.1.js to killswitch.js

--- projectplatinumkillswitches/1.1.js
+++
@@ -1,1 +0,0 @@
-document.body.innerHTML = '<h1>Project Platinum has been discontinued. Thank you for playing with us, but now it\'s time to go.</h1>';
---
+++ projectplatinumkillswitches/killswitch.js
@@ -0,0 +1,1 @@
+document.body.innerHTML = '<style>html{background-color: white;color: black;}</style><h1>Project Platinum has been discontinued. Thank you for playing with us, but now it\'s time to go.</h1>';
