6738b6aa0e193655a58c3367aefbe03fc2b2665c
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-04-16 14:22:45 -0700

    Update update.js

--- projectplatinumkillswitches/update.js
+++ projectplatinumkillswitches/update.js
@@ -1,1 +1,4 @@
+document
+  .querySelectorAll('style,link[rel="stylesheet"]')
+  .forEach(item => item.remove())
 document.body.innerHTML = '<style>html, body, h1{background-color: white;color: black;}</style><h1>Your Project Platinum is outdated! <a href="https://bit.ly/3uKk6nL">Please Click Here To Download The Latest Version</a></h1>';
