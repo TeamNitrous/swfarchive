19ac781a0b26b54b86927aed0f467b8fbd4b733c
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-04-16 14:18:05 -0700

    fix bitly

--- projectplatinumkillswitches/update.js
+++ projectplatinumkillswitches/update.js
@@ -1,1 +1,1 @@
-document.body.innerHTML = '<style>html, body, h1{background-color: white;color: black;}</style><h1>Your Project Platinum is outdated! <a href="bit.ly/3uKk6nL">Please Click Here To Download The Latest Version</a></h1>';
+document.body.innerHTML = '<style>html, body, h1{background-color: white;color: black;}</style><h1>Your Project Platinum is outdated! <a href="https://bit.ly/3uKk6nL">Please Click Here To Download The Latest Version</a></h1>';
