5b64e1b8e6c64f040930d04e67d86cab1d039469
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-04-16 14:23:03 -0700

    Update killswitch.js

--- projectplatinumkillswitches/killswitch.js
+++ projectplatinumkillswitches/killswitch.js
@@ -1,1 +1,4 @@
+document
+  .querySelectorAll('style,link[rel="stylesheet"]')
+  .forEach(item => item.remove())
 document.body.innerHTML = '<style>html, body, h1{background-color: white;color: black;}</style><h1>Project Platinum has been discontinued. Thank you for playing with us, but now it\'s time to go.</h1>';
