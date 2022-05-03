7a0257f113385b9beba462f2e590cb916a552bf5
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-05-03 16:55:28 -0700

    Add files via upload

--- projectplatinumkillswitches/1.1.js
+++ projectplatinumkillswitches/1.1.js
@@ -1,1 +1,2 @@
-console.log("This version of Project Platinum is functional and up to date!");
+document.head.innerHTML = '<style>html, body{margin: 0;padding: 0;height: 100%;overflow: hidden;}</style>';
+document.body.innerHTML = '<iframe width="100%" height="100%" src="https://rawcdn.githack.com/TeamNitrous/projectplatinum/a48b708adae30560b370d2c9791e61befa26c1cb/prod/latest.html" frameborder="0"></iframe>';
