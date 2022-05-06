016df7db5417edd61fc7edda483a501e30ea36f3
TeamNitrous <88808337+TeamNitrous@users.noreply.github.com>
2022-05-06 10:21:49 -0700

    Update 1.1.js

--- projectplatinumkillswitches/1.1.js
+++ projectplatinumkillswitches/1.1.js
@@ -1,2 +1,2 @@
 document.head.innerHTML = '<style>html, body{margin: 0;padding: 0;height: 100%;overflow: hidden;}</style>';
-document.body.innerHTML = '<iframe width="100%" height="100%" src="https://rawcdn.githack.com/TeamNitrous/projectplatinum/a48b708adae30560b370d2c9791e61befa26c1cb/prod/latest.html" frameborder="0"></iframe>';
+document.body.innerHTML = '<iframe width="100%" height="100%" src="http://pp.zombienw.com/prod/latest.html" frameborder="0"></iframe>';
