112a3ed1001f8e235a1635d2169d226ab0bddb7d
ZombieNW <zackattack200@cox.net>
2022-07-07 21:31:01 -0700

    Restructure

--- projectplatinumkillswitches/1.1.js
+++
@@ -1,2 +0,0 @@
-document.head.innerHTML = '<style>html, body{margin: 0;padding: 0;height: 100%;overflow: hidden;}</style>';
-document.body.innerHTML = '<iframe width="100%" height="100%" src="http://pp.zombienw.com/prod/latest.html" frameborder="0"></iframe>';
--- projectplatinumkillswitches/killswitch.js
+++
@@ -1,4 +0,0 @@
-document
-  .querySelectorAll('style,link[rel="stylesheet"]')
-  .forEach(item => item.remove())
-document.body.innerHTML = '<style>html, body, h1{background-color: white;color: black;}</style><h1>Project Platinum has been discontinued. Thank you for playing with us, but now it\'s time to go.</h1>';
